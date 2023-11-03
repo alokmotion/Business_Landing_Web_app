4a70bd600d4fecffb4013c9c07877d08ee479103
alokmotion <alokpatel406@gmail.com>
2023-11-03 18:10:14 +0530

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,15 @@
+import { card } from "../assets"
+import styles,{layout} from "../style"
+import Button from "./Button"
+
+
+
+const CardDeal = () => {
+  return (
+    <section className={layout.section}>
+      
+    </section>
+  )
+}
+
+export default CardDeal
